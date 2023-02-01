aaa24b660cf00225e4949c4e68de04a62e9befea
sssenya <senia7@bk.ru>
2023-02-01 14:31:33 +0300

    Обновлен скрипт для заморозки площадей помещений. Добавлено получение всех параметров из платформы.

--- АР.tab/Квартирография.panel/Помещения.pulldown/Заморозка площадей.pushbutton/script.py
+++ АР.tab/Квартирография.panel/Помещения.pulldown/Заморозка площадей.pushbutton/script.py
@@ -16,31 +16,43 @@ clr.ImportExtensions(dosymep.Bim4Everyone)
 
 from dosymep_libs.bim4everyone import *
 from dosymep.Bim4Everyone.SharedParams import *
+from dosymep.Bim4Everyone.Templates import ProjectParameters
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
 
-def freeze_area(element, param, guid_str):
-    value = element.GetParamValueOrDefault(param)
-    guid = Guid(guid_str)
-    element.SetParamValue(element.get_Parameter(guid).Definition.Name, value)
-
-
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
+    area_params = [SharedParamsConfig.Instance.ApartmentLivingArea,
+                   SharedParamsConfig.Instance.ApartmentAreaRatio,
+                   SharedParamsConfig.Instance.ApartmentArea,
+                   SharedParamsConfig.Instance.ApartmentAreaNoBalcony,
+                   SharedParamsConfig.Instance.ApartmentFullArea,
+                   SharedParamsConfig.Instance.RoomAreaWithRatio,
+                   SharedParamsConfig.Instance.RoomArea]
+
+    area_fix_params = [SharedParamsConfig.Instance.ApartmentLivingAreaFix,
+                       SharedParamsConfig.Instance.ApartmentAreaRatioFix,
+                       SharedParamsConfig.Instance.ApartmentAreaFix,
+                       SharedParamsConfig.Instance.ApartmentAreaNoBalconyFix,
+                       SharedParamsConfig.Instance.ApartmentFullAreaFix,
+                       SharedParamsConfig.Instance.RoomAreaWithRatioFix,
+                       SharedParamsConfig.Instance.RoomAreaFix]
+
     rooms = FilteredElementCollector(doc)
     rooms.OfCategory(BuiltInCategory.OST_Rooms).ToElements()
-    with revit.Transaction("BIM: Заморозить площади помещений") :
-        for room in rooms:
-            freeze_area(room, SharedParamsConfig.Instance.ApartmentLivingArea, '421ec146-2f9b-48ae-9bcc-1f478c115e7e')
-            freeze_area(room, SharedParamsConfig.Instance.ApartmentAreaRatio, '77713404-35ef-4c8a-a5c4-c6f3d4da16c2')
-            freeze_area(room, SharedParamsConfig.Instance.ApartmentArea, '3d6c5084-d6ab-490e-baa6-f8d119a0d628')
-            freeze_area(room, SharedParamsConfig.Instance.ApartmentAreaNoBalcony, '8226f116-c19e-4797-a3bb-55ac79acdf44')
-            # freeze_area(room, SharedParamsConfig.Instance.ApartmentFullArea, 'bc1bf705-37a8-404e-af7c-ca072168b994')
-            freeze_area(room, SharedParamsConfig.Instance.RoomAreaWithRatio, '46bc1213-6d5a-4164-84d0-598a9abcf70d')
-            freeze_area(room, SharedParamsConfig.Instance.RoomArea, '64bf4d4d-6ef3-4cfd-b452-eccdde94a8ad')
+
+    if rooms.FirstElement():
+        for param in area_fix_params:
+            if not rooms.FirstElement().IsExistsParam(param):
+                ProjectParameters.Create(doc.Application).SetupRevitParam(doc, param)
+
+        with revit.Transaction("BIM: Заморозить площади помещений"):
+            for room in rooms:
+                for param, param_fix in zip(area_params, area_fix_params):
+                    room.SetParamValue(param_fix, param)
 
 
 script_execute()
