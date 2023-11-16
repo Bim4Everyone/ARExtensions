13126fd6639e806bc69396b6f92a30228df1162b
Данил Мануковский <bossdarko777@gmail.com>
2023-11-16 10:39:52 +0300

    Изменил bundle, доработка окна, исправления в скрипте

--- АР.tab/РД.panel/Отделка пола.pushbutton/script.py
+++ АР.tab/РД.panel/Отделка пола.pushbutton/script.py
@@ -2,9 +2,8 @@
 
 import clr
 import datetime
-
+import re
 from System.Collections.Generic import *
-from System.Windows.Input import ICommand
 
 clr.AddReference("dosymep.Revit.dll")
 clr.AddReference("dosymep.Bim4Everyone.dll")
@@ -41,7 +40,28 @@ def convert_value(value):
         return UnitUtils.ConvertToInternalUnits(value, UnitTypeId.Millimeters)
 
 
-class RoomContour():
+# def check_active_view_not_plan():
+#     active_view_type = doc.ActiveView.ViewType
+#     return active_view_type != ViewType.FloorPlan
+
+
+class RevitRepository:
+    def __init__(self, doc):
+        self.doc = doc
+        self.__floor_types = self.__collect_floor_types()
+
+    @reactive
+    def floor_types(self):
+        return self.__floor_types
+
+    def __collect_floor_types(self):
+        all_floor_types = FilteredElementCollector(doc) \
+            .WhereElementIsElementType() \
+            .OfCategory(BuiltInCategory.OST_Floors)
+        return elements_to_list(all_floor_types)
+
+
+class RoomContour:
     def __init__(self, room):
         self.room = room
 
@@ -125,53 +145,79 @@ def collect_rooms():
     return correct_rooms
 
 
-def collect_floor_types():
-    all_floor_types = FilteredElementCollector(doc) \
-        .WhereElementIsElementType() \
-        .OfCategory(BuiltInCategory.OST_Floors)
-    return all_floor_types
-
-
-class CollectRevitInfo:
-    def __init__(self):
-        self.__floor_types = self.collect_floor_types()
-
-    @reactive
-    def floor_types(self):
-        return self.__floor_types
-
-    def collect_floor_types(self):
-        all_floor_types = FilteredElementCollector(doc) \
-            .WhereElementIsElementType() \
-            .OfCategory(BuiltInCategory.OST_Floors)
-        return all_floor_types
-
-
-def check_active_view_not_plan():
-    active_view_type = doc.ActiveView.ViewType
-    return active_view_type != ViewType.FloorPlan
-
-
 # def floor_create(curve_loop, level_id):
 #     floor_type = (FilteredElementCollector(doc)
 #                   .WhereElementIsElementType()
 #                   .OfCategory(BuiltInCategory.OST_Floors)
 #                   .FirstElementId())
 #     Floor.Create(doc, curve_loop, floor_type, level_id)
+class CreateFloorsByRooms:
+    def __init__(self):
+        self.__rooms = collect_rooms()
 
-def floor_create(room, floor_type, level_offset=0):
-    '''
-    Создает перекрытие на основе CurveLoop помещения и его уровня, заданным типоразмером перекрытия со смещением
-    от уровня (опционально)
-    room: помещение, на основе которого будет создано перекрытие
-    floor_type: типоразмер перекрытия, который будет указан для создания
-    level_offset: смещение от уровня (по умолчанию 0)
-    '''
-    curve_loop = RoomContour(room).get_curves_of_room()
-    level_id = room.LevelId
-    current_floor = Floor.Create(doc, curve_loop, floor_type.Id, level_id)
-    level_offset = convert_value(level_offset)
-    floor_offset = current_floor.get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).Set(level_offset)
+    def rooms(self):
+        return self.__rooms
+
+    def floor_create(self, room, floor_type, level_offset=0):
+        '''
+        Создает перекрытие на основе CurveLoop помещения и его уровня, заданным типоразмером перекрытия со смещением
+        от уровня (опционально)
+        room: помещение, на основе которого будет создано перекрытие
+        floor_type: типоразмер перекрытия, который будет указан для создания
+        level_offset: смещение от уровня (по умолчанию 0)
+        '''
+        curve_loop = RoomContour(room).get_curves_of_room()
+        level_id = room.LevelId
+        current_floor = Floor.Create(doc, curve_loop, floor_type.Id, level_id)
+        converted_level_offset = convert_value(level_offset)
+        floor_offset = current_floor.get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).Set(
+            converted_level_offset)
+
+    def create_floors_by_rooms_on_view(self, floor_type, level_offset=0):
+        '''
+        Создает перекрытия последовательно по помещениям в выборке, используя функцию создания перекрытия по помещению
+        floor_type: типоразмер перекрытия, который будет указан для создания
+        level_offset: смещение от уровня (по умолчанию 0)
+        '''
+        all_rooms = self.rooms()
+        with Transaction(doc, "BIM: Создание перекрытий") as t:
+            t.Start()
+            for room in all_rooms:
+                self.floor_create(room, floor_type, level_offset)
+            t.Commit()
+
+
+class CreateFloorsByRoomsOnViewCommand(ICommand):
+    CanExecuteChanged, _canExecuteChanged = pyevent.make_event()
+
+    def __init__(self, view_model, *args):
+        ICommand.__init__(self, *args)
+        self.__view_model = view_model
+        self.__view_model.PropertyChanged += self.ViewModel_PropertyChanged
+        self.__create_floors_by_view = CreateFloorsByRooms()
+
+    def add_CanExecuteChanged(self, value):
+        self.CanExecuteChanged += value
+
+    def remove_CanExecuteChanged(self, value):
+        self.CanExecuteChanged -= value
+
+    def OnCanExecuteChanged(self):
+        # В Python при работе с событиями нужно явно
+        # передавать импорт в обработчике события
+        from System import EventArgs
+        self._canExecuteChanged(self, EventArgs.Empty)
+
+    def ViewModel_PropertyChanged(self, sender, e):
+        self.OnCanExecuteChanged()
+
+    def CanExecute(self, parameter):
+        return True
+
+    def Execute(self, parameter):
+        if not self.__view_model.is_checked_selected:
+            self.__create_floors_by_view.create_floors_by_rooms_on_view(self.__view_model.selected_floor_type,
+                                                                        self.__view_model.level_offset)
 
 
 class MainWindow(WPFWindow):
@@ -180,6 +226,9 @@ class MainWindow(WPFWindow):
         self.xaml_source = op.join(op.dirname(__file__), "MainWindow.xaml")
         super(MainWindow, self).__init__(self.xaml_source)
 
+    def ButtonOk_Click(self, sender, e):
+        self.DialogResult = True
+
 
 class MainWindowViewModel(Reactive):
     def __init__(self, revit_info):
@@ -187,9 +236,48 @@ class MainWindowViewModel(Reactive):
         self.__revit_info = revit_info
         self.__floor_types = revit_info.floor_types
 
+        if len(self.__floor_types) > 0:
+            self.__selected_floor_type = self.floor_types[0]
+
+        self.__level_offset = 0
+        self.__create_floors_by_rooms_on_view = CreateFloorsByRoomsOnViewCommand(self)
+        self.__is_checked_selected = True
+
     @reactive
     def floor_types(self):
         return self.__floor_types
+
+    @reactive
+    def selected_floor_type(self):
+        return self.__selected_floor_type
+
+    @property
+    def create_floors_by_rooms_on_view(self):
+        return self.__create_floors_by_rooms_on_view
+
+    @reactive
+    def selected_floor_type(self):
+        return self.__selected_floor_type
+
+    @selected_floor_type.setter
+    def selected_floor_type(self, value):
+        self.__selected_floor_type = value
+
+    @reactive
+    def level_offset(self):
+        return self.__level_offset
+
+    @level_offset.setter
+    def level_offset(self, value):
+        self.__level_offset = value
+
+    @reactive
+    def is_checked_selected(self):
+        return self.__is_checked_selected
+
+    @is_checked_selected.setter
+    def is_checked_selected(self, value):
+        self.__is_checked_selected = value
 
 
 @notification()
@@ -202,27 +290,29 @@ def script_execute(plugin_logger):
         forms.alert("В проекте отсутствуют размещенные помещения")
         return
 
-    # Проверка принадлежности активного вида не к Плану этажа
-    if check_active_view_not_plan():
-        forms.alert("Для запуска плагина перейдите на план этажа!")
-        return
+    # # Проверка принадлежности активного вида не к Плану этажа
+    # if check_active_view_not_plan():
+    #     forms.alert("Для запуска плагина перейдите на план этажа!")
+    #     return
 
-    all_floors = collect_floor_types()
-    floor_types = elements_to_list(all_floors)
+    # all_floors = collect_floor_types()
+    # floor_types = elements_to_list(all_floors)
+    revit_info = RevitRepository(doc)
     main_window = MainWindow()
-    revit_info = CollectRevitInfo()
     main_window.DataContext = MainWindowViewModel(revit_info)
     main_window.show_dialog()
-    with Transaction(doc, "BIM: Отделка пола") as t:
-        t.Start()
-        for room in all_rooms:
-            floor_type = floor_types[0]
-            # curve = RoomContour(room).get_curves_of_room()
-            # level_id = room.LevelId
-            # floor_create(curve, level_id)
-            floor_create(room, floor_type)
-
-        t.Commit()
+    if not main_window.DialogResult:
+        script.exit()
+    # with Transaction(doc, "BIM: Отделка пола") as t:
+    #     t.Start()
+    #     for room in all_rooms:
+    #         floor_type = floor_types[0]
+    #         # curve = RoomContour(room).get_curves_of_room()
+    #         # level_id = room.LevelId
+    #         # floor_create(curve, level_id)
+    #         floor_create(room, floor_type)
+    #
+    #     t.Commit()
 
 
 script_execute()
